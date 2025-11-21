431a78d4c96642a807ab8c874ea6641fe5e7eb68
BeheadedBadger <ZarradeRuijter@gmail.com>
2025-11-21 15:57:37 +0100

    Tiles and basic camera controls
    Free texture pack for now!

--- ProtoLife0.01/Assets/Gameplay/Controls/PlayerInput.cs
+++ ProtoLife0.01/Assets/Gameplay/Controls/PlayerInput.cs
@@ -7,6 +7,20 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private LayerMask placementLayerMask;
     [SerializeField] private GameObject mouseIndicator, cellIndicator;
     [SerializeField] Grid grid;
+    [SerializeField] GameObject gridVisualLvl0;
+    [SerializeField] GameObject gridVisualLvl1;
+    [SerializeField] GameObject gridVisualLvl2;
+    [SerializeField] GameObject gridVisualLvl3;
+
+    [SerializeField] GameObject CameraRig;
+    float cameraMovementSpeed = 0.1f;
+    float cameraMovementTime = 5;
+    Vector3 newPosition;
+
+    private void Start()
+    {
+        newPosition = CameraRig.transform.position;
+    }
 
     void Update()
     {
@@ -14,6 +28,41 @@ public class PlayerInput : MonoBehaviour
         Vector3Int gridPos = grid.WorldToCell(mousePos);
         mouseIndicator.transform.position = mousePos;
         cellIndicator.transform.position = grid.CellToWorld(gridPos);
+
+        GridLevelDisplay(gridPos);
+        HandleCameraMovement();
+    }
+
+    private void GridLevelDisplay(Vector3Int gridPos)
+    {
+        if (gridPos.y == 3 && gridVisualLvl3.activeSelf == false)
+        {
+            gridVisualLvl0.SetActive(false);
+            gridVisualLvl1.SetActive(false);
+            gridVisualLvl2.SetActive(false);
+            gridVisualLvl3.SetActive(true);
+        }
+        else if (gridPos.y == 2 && gridVisualLvl2.activeSelf == false)
+        {
+            gridVisualLvl0.SetActive(false);
+            gridVisualLvl1.SetActive(false);
+            gridVisualLvl2.SetActive(true);
+            gridVisualLvl3.SetActive(false);
+        }
+        else if (gridPos.y == 1 && gridVisualLvl1.activeSelf == false)
+        {
+            gridVisualLvl0.SetActive(false);
+            gridVisualLvl1.SetActive(true);
+            gridVisualLvl2.SetActive(false);
+            gridVisualLvl3.SetActive(false);
+        }
+        else if (gridPos.y == 0 && gridVisualLvl0.activeSelf == false)
+        {
+            gridVisualLvl0.SetActive(true);
+            gridVisualLvl1.SetActive(false);
+            gridVisualLvl2.SetActive(false);
+            gridVisualLvl3.SetActive(false);
+        }
     }
 
     public Vector3 GetSelectedMapPosition()
@@ -28,5 +77,34 @@ public class PlayerInput : MonoBehaviour
         }
         Debug.Log(lastPosition);
         return lastPosition;
+    }
+
+    void HandleCameraMovement()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += (transform.forward * cameraMovementSpeed);
+            newPosition += (transform.right * cameraMovementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += (transform.forward * -cameraMovementSpeed);
+            newPosition += (transform.right * -cameraMovementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += (transform.right * cameraMovementSpeed);
+            newPosition += (transform.forward * -cameraMovementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition += (transform.right * -cameraMovementSpeed);
+            newPosition += (transform.forward * cameraMovementSpeed);
+        }
+
+        CameraRig.transform.position = Vector3.Lerp(CameraRig.transform.position, newPosition, Time.deltaTime * cameraMovementTime);
     }
 }
